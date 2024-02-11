22cd675045a297b52f1da7f9b180cade7c27997b
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-11 11:47:57 +0100

    Faster Factorial

--- Arrays To Do 8/main.js
+++ Arrays To Do 8/main.js
@@ -3,3 +3,14 @@ function iSigma(num) {
     if (num <= 0) return 0;
     return (num * (num + 1)) / 2;
   }
+
+  // Faster Factorial
+function Factorial(num) {
+    if (num <= 1) return 1;
+    
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
