20466dda843646f6320bbdf722f24b417b5026b5
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-11 11:53:38 +0100

    Tricky Tribonacci

--- Arrays To Do 8/main.js
+++ Arrays To Do 8/main.js
@@ -30,3 +30,22 @@ function iFib(num) {
     
     return current;
   }
+
+  // Tricky Tribonacci
+function iTrib(num) {
+    if (num <= 0) return 0;
+    if (num <= 2) return 1;
+    
+    let prev1 = 0;
+    let prev2 = 0;
+    let current = 1;
+    
+    for (let i = 3; i <= num; i++) {
+      const temp = prev1 + prev2 + current;
+      prev1 = prev2;
+      prev2 = current;
+      current = temp;
+    }
+    
+    return current;
+  }
