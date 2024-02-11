98320c75f3f3b960a1771c46e87a06c26aee79d3
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-11 11:48:51 +0100

    Fancy Fibonacci

--- Arrays To Do 8/main.js
+++ Arrays To Do 8/main.js
@@ -14,3 +14,19 @@ function Factorial(num) {
     }
     return result;
   }
+
+  // Fancy Fibonacci
+function iFib(num) {
+    if (num <= 0) return 0;
+    
+    let prev = 0;
+    let current = 1;
+    
+    for (let i = 2; i <= num; i++) {
+      const temp = prev + current;
+      prev = current;
+      current = temp;
+    }
+    
+    return current;
+  }
