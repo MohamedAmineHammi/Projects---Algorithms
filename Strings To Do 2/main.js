feceef3694d87dd7c84d2c955f4e823943d020bc
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-14 09:59:43 +0100

    Integer to Roman Numerals

--- Strings To Do 2/main.js
+++ Strings To Do 2/main.js
@@ -22,3 +22,34 @@ function reverseString(str) {
   
   let strings = ["Nope!", "Its", "Kris", "starting", "with", "K!", "(instead", "of", "Chris", "with", "C)", "."];
   console.log(removeEvenLengthStrings(strings)); // Output: ["Nope!", "Its", "Chris", "."]
+
+  // 3- Integer to Roman Numerals:
+  function integerToRoman(num) {
+    const romanNumerals = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+  
+    let roman = "";
+    for (let symbol in romanNumerals) {
+      while (num >= romanNumerals[symbol]) {
+        roman += symbol;
+        num -= romanNumerals[symbol];
+      }
+    }
+    return roman;
+  }
+  
+  console.log(integerToRoman(349)); // Output: "CCCIL"
+  console.log(integerToRoman(444)); // Output: "CDXLIV"
