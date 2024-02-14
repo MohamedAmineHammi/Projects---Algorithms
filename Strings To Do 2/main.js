1c34311e421ad0b8750a89225bbf7dbc410c4161
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-14 10:01:16 +0100

    Roman Numerals to Integer

--- Strings To Do 2/main.js
+++ Strings To Do 2/main.js
@@ -53,3 +53,37 @@ function reverseString(str) {
   
   console.log(integerToRoman(349)); // Output: "CCCIL"
   console.log(integerToRoman(444)); // Output: "CDXLIV"
+
+  // 4- Roman Numerals to Integer:
+  function romanToInteger(roman) {
+    const romanNumerals = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+  
+    let num = 0;
+    for (let i = 0; i < roman.length; i++) {
+      if (romanNumerals[roman[i]] < romanNumerals[roman[i + 1]]) {
+        num += romanNumerals[roman[i + 1]] - romanNumerals[roman[i]];
+        i++;
+      } else {
+        num += romanNumerals[roman[i]];
+      }
+    }
+    return num;
+  }
+  
+  console.log(romanToInteger("III")); // Output: 3
+  console.log(romanToInteger("DCIX")); // Output: 609
+  console.log(romanToInteger("MXDII")); // Output: 1492
