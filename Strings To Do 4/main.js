6bd806ee069575d50cc65164ba28b337f3ec4d91
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-16 10:00:49 +0100

    D Gets Jiggy

--- Strings To Do 4/main.js
+++ Strings To Do 4/main.js
@@ -7,3 +7,13 @@ function isWordAlphabetical(word) {
   // Example usage
   console.log(isWordAlphabetical("facetiously")); // Output: true
   console.log(isWordAlphabetical("arseniously")); // Output: false
+
+  // 2- D Gets Jiggy:
+  function coolGreeting(name) {
+    const newWord = name.slice(1);
+    const capitalized = newWord.charAt(0).toUpperCase() + newWord.slice(1);
+    return capitalized + " to the " + name.charAt(0) + "!";
+  }
+  
+  // Example usage
+  console.log(coolGreeting("Dylan")); // Output: "Ylan to the D!"
