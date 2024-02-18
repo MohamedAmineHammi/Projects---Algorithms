1fae1eb3ad0a662742a46e04a592dee01a59d9e4
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-18 08:59:21 +0100

    Coin Change with Object

---
+++ Strings To Do 5/main.js
@@ -0,0 +1,24 @@
+// 1- Coin Change with Object
+function coinChange(cents) {
+    var coins = {
+      quarters: 0,
+      dimes: 0,
+      nickels: 0,
+      pennies: 0
+    };
+  
+    coins.quarters = Math.floor(cents / 25);
+    cents %= 25;
+  
+    coins.dimes = Math.floor(cents / 10);
+    cents %= 10;
+  
+    coins.nickels = Math.floor(cents / 5);
+    cents %= 5;
+  
+    coins.pennies = cents;
+  
+    return coins;
+  }
+  
+  console.log(coinChange(97));
