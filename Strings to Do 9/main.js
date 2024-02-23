66c589c0df52b4117559b2ca1fcbcab8de1df6c7
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-23 10:01:26 +0100

    Censor

--- Strings to Do 9/main.js
+++ Strings to Do 9/main.js
@@ -3,3 +3,12 @@ function rotateString(str, num) {
     const rotated = str.slice(-num) + str.slice(0, -num);
     return rotated;
   }
+
+  // Censor
+function censorString(str, naughtyWords) {
+    for (let word of naughtyWords) {
+      const regex = new RegExp(word, 'gi');
+      str = str.replace(regex, 'x'.repeat(word.length));
+    }
+    return str;
+  }
