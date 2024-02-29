ddec1d1b05290aef09f081ac6dcf59233bfb3244
MohamedAmineHammi <eminehammi@gmail.com>
2024-02-29 22:18:10 +0100

    All Loosely Interleaved Strings

--- Strings To Do 12/main.js
+++ Strings To Do 12/main.js
@@ -15,3 +15,30 @@ function areStringsLooselyInterleaved(str1, str2, str3) {
   console.log(areStringsLooselyInterleaved("dne", "ail", "daniel")); // true
   console.log(areStringsLooselyInterleaved("dne", "ail", "dalein")); // false
   console.log(areStringsLooselyInterleaved("dne", "ail", "ddaanneeiill")); // false
+
+  // All Loosely Interleaved Strings:
+  function allLooselyInterleavedStrings(str1, str2) {
+    const result = [];
+  
+    function backtrack(s1, s2, current) {
+      if (s1.length === 0 && s2.length === 0) {
+        result.push(current);
+        return;
+      }
+  
+      if (s1.length > 0) {
+        backtrack(s1.slice(1), s2, current + s1[0]);
+      }
+  
+      if (s2.length > 0) {
+        backtrack(s1, s2.slice(1), current + s2[0]);
+      }
+    }
+  
+    backtrack(str1, str2, '');
+  
+    return [...new Set(result)];
+  }
+  
+  // Example usage:
+  console.log(allLooselyInterleavedStrings("ab", "yz")); // ["abyz","aybz","ayzb","yabz","yazb","yzab"]
