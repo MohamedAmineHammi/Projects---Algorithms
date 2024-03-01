18abe87b46f1671ba517094e935d0c35c69e034b
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-01 08:39:57 +0100

    String Decode

--- Strings To Do 13/main.js
+++ Strings To Do 13/main.js
@@ -18,3 +18,24 @@ function encodeString(str) {
   // Example usage:
   console.log(encodeString("aaaabbcddd")); // Output: "a4b2c1d3"
   console.log(encodeString("bb")); // Output: "bb"
+
+  // 2-String Decode:
+  function decodeString(str) {
+    let decoded = '';
+    let count = '';
+  
+    for (let i = 0; i < str.length; i++) {
+      if (isNaN(parseInt(str[i]))) {
+        decoded += str[i].repeat(parseInt(count) || 1);
+        count = '';
+      } else {
+        count += str[i];
+      }
+    }
+  
+    return decoded;
+  }
+  
+  // Example usage:
+  console.log(decodeString("a3b2c1d3")); // Output: "aaabbcddd"
+
