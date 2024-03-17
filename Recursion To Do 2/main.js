c7dd21d327482afc21a8d115d24fb3e74c87190b
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-17 23:50:05 +0100

    Recursive "Tribonacci"

--- Recursion To Do 2/main.js
+++ Recursion To Do 2/main.js
@@ -12,3 +12,16 @@ function rFib(num) {
   console.log(rFib(5));    // Output: 5
   console.log(rFib(3.65)); // Output: 2
   console.log(rFib(-2));   // Output: 0
+
+  // Recursive "Tribonacci"
+function rTrib(num) {
+    if (num === 0 || num === 1) return 0;
+    if (num === 2) return 1;
+  
+    return rTrib(num - 1) + rTrib(num - 2) + rTrib(num - 3);
+  }
+  
+  console.log(rTrib(3)); // Output: 1
+  console.log(rTrib(4)); // Output: 2
+  console.log(rTrib(5)); // Output: 4
+  console.log(rTrib(6)); // Output: 7
