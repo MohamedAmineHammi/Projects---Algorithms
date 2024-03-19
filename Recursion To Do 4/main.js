a47bbf66bcf8a75ca2792c1fc14264a3052437e7
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-20 00:51:50 +0100

    Got Any Grapes?!

--- Recursion To Do 4/main.js
+++ Recursion To Do 4/main.js
@@ -5,3 +5,21 @@ function getListLength(node) {
     }
     return 1 + getListLength(node.next);
   }
+
+  //Got Any Grapes?!?
+  function getMaxGrapes(baggies) {
+    const n = baggies.length;
+    if (n === 0) {
+      return 0;
+    }
+    if (n === 1) {
+      return baggies[0];
+    }
+    const dp = new Array(n);
+    dp[0] = baggies[0];
+    dp[1] = Math.max(baggies[0], baggies[1]);
+    for (let i = 2; i < n; i++) {
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + baggies[i]);
+    }
+    return dp[n - 1];
+  }
