33f99f05c317ad31d93ba2ecdb7f60af647dabb5
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-20 00:53:59 +0100

    Telephone Words

--- Recursion To Do 4/main.js
+++ Recursion To Do 4/main.js
@@ -34,3 +34,42 @@ function getListLength(node) {
     }
     return 1 + collatzIterations(3 * num + 1);
   }
+
+  //Telephone Words:
+  function getTelephoneWords(number) {
+    const mappings = {
+      '0': 'O',
+      '1': 'I',
+      '2': 'ABC',
+      '3': 'DEF',
+      '4': 'GHI',
+      '5': 'JKL',
+      '6': 'MNO',
+      '7': 'PQRS',
+      '8': 'TUV',
+      '9': 'WXYZ'
+    };
+  
+    const result = [];
+  
+    function backtrack(combination, digits) {
+      if (digits.length === 0) {
+        result.push(combination);
+      } else {
+        const currentDigit = digits[0];
+        const letters = mappings[currentDigit];
+        for (let i = 0; i < letters.length; i++) {
+          const letter = letters[i];
+          backtrack(combination + letter, digits.slice(1));
+        }
+      }
+    }
+  
+    backtrack('', number.split(''));
+    return result;
+  }
+  
+  const phoneNumber = '818-2612';
+  const telephoneWords = getTelephoneWords(phoneNumber.replace(/-/g, ''));
+  console.log(telephoneWords.length); // Output: 243
+  console.log(telephoneWords); // Output: Array of 243 different strings
