82e4ebc675a95ca2d6b5b75085513a87e972b79a
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-21 00:22:08 +0100

    Climbing Stairs

--- Recursion To Do 5/main.js
+++ Recursion To Do 5/main.js
@@ -50,3 +50,55 @@ function risingSquare(n) {
   }
   
   console.log(stringAnagrams("lim")); // Output: ["lim", "lmi", "ilm", "iml", "mli", "mil"]
+
+  //Climbing Stairs:
+// First problem: all ways to walk the stairs
+function climbStairs(n) {
+    if (n === 0) {
+      return [[]];
+    } else if (n === 1) {
+      return [[1]];
+    } else {
+      const allWays = [];
+      const oneStepWays = climbStairs(n - 1);
+      const twoStepWays = climbStairs(n - 2);
+      for (let i = 0; i < oneStepWays.length; i++) {
+        const way = oneStepWays[i];
+        allWays.push([1, ...way]);
+      }
+      for (let i = 0; i < twoStepWays.length; i++) {
+        const way = twoStepWays[i];
+        allWays.push([2, ...way]);
+      }
+      return allWays;
+    }
+  }
+  
+  console.log(climbStairs(4)); // Output: [[1, 1, 1, 1], [1, 1, 2], [1, 2, 1], [2, 1, 1], [2, 2]]
+  
+  // Second problem: ways that end with a right step
+  function climbStairsEndRight(n) {
+    const allWays = climbStairs(n);
+    return allWays.filter((way) => way[way.length - 1] === 2);
+  }
+  
+  console.log(climbStairsEndRight(4)); // Output: [[1, 1, 1, 1], [2, 2]]
+  
+  // Third problem: ways where total left steps equal total right steps
+  function climbStairsEqualSteps(n) {
+    const allWays = climbStairs(n);
+    return allWays.filter((way) => {
+      let leftSteps = 0;
+      let rightSteps = 0;
+      for (let i = 0; i < way.length; i++) {
+        if (way[i] === 1) {
+          leftSteps++;
+        } else {
+          rightSteps++;
+        }
+      }
+      return leftSteps === rightSteps;
+    });
+  }
+  
+  console.log(climbStairsEqualSteps(4)); // Output: [[1, 1, 1, 1], [1, 2, 1], [2, 2]]
