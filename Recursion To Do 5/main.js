45f2205176f40ff390533cdc56b1cff50f6bb08a
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-21 00:20:45 +0100

    Binary String Expansion

--- Recursion To Do 5/main.js
+++ Recursion To Do 5/main.js
@@ -11,3 +11,22 @@ function risingSquare(n) {
   
   console.log(risingSquare(5)); // Output: 1, 9, 25, 16, 4
   console.log(risingSquare(8)); // Output: 1, 9, 25, 49, 64, 36, 16, 4
+
+  //Binary String Expansion:
+  function binStrExpand(str) {
+    if (str.length === 0) {
+      return [""];
+    } else {
+      const firstChar = str[0];
+      const restStr = str.slice(1);
+      if (firstChar === "?") {
+        const expandedWithZero = binStrExpand(restStr).map((s) => "0" + s);
+        const expandedWithOne = binStrExpand(restStr).map((s) => "1" + s);
+        return expandedWithZero.concat(expandedWithOne);
+      } else {
+        return binStrExpand(restStr).map((s) => firstChar + s);
+      }
+    }
+  }
+  
+  console.log(binStrExpand("1?0?")); // Output: ["1000", "1001", "1100", "1101"]
