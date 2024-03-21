5c1add55d9e29d135adf749f93db2326dc58d6b8
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-22 00:26:05 +0100

    All Valid N Pairs of Parens

--- Recursion To Do 6/main.js
+++ Recursion To Do 6/main.js
@@ -23,3 +23,28 @@ function sumOfSquares(num) {
   
   sumOfSquares(10);
   sumOfSquares(30);
+
+  //All Valid N Pairs of Parens:
+  function generateParentheses(n) {
+    const result = [];
+  
+    function backtrack(str, open, close) {
+      if (str.length === n * 2) {
+        result.push(str);
+        return;
+      }
+  
+      if (open < n) {
+        backtrack(str + '(', open + 1, close);
+      }
+  
+      if (close < open) {
+        backtrack(str + ')', open, close + 1);
+      }
+    }
+  
+    backtrack('', 0, 0);
+    return result;
+  }
+  
+  console.log(generateParentheses(2));
