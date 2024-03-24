8e75ca86caa7da4c8236740fe13ad611b6f9a8e0
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-24 01:42:07 +0100

    corrected Code

---
+++ Where’s the Bug/main.js
@@ -0,0 +1,30 @@
+// corrected Code
+
+function rFib(num) {
+    if (num <= 1) {
+        return num;
+    }
+    return rFib(num - 1) + rFib(num - 2);
+}
+
+function rListLength(node) {
+    if (!node) {
+        return 0;
+    } else {
+        return 1 + rListLength(node.next);
+    }
+}
+
+function rSigma(num) {
+    if (num === 0) {
+        return 0;
+    }
+    return num + rSigma(num - 1);
+}
+
+function rFactorial(num) {
+    if (num === 0) {
+        return 1;
+    }
+    return rFactorial(num - 1) * num;
+}
