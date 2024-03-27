cdd0dbfa004891e576543976b9b63a459a196205
MohamedAmineHammi <eminehammi@gmail.com>
2024-03-27 02:18:36 +0100

    Insert a value into BST

--- Trees To Do 3/main.js
+++ Trees To Do 3/main.js
@@ -12,3 +12,33 @@ class BST {
     constructor() {
       this.root = null;
     }
+}
+
+  // Insert a value into BST
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          if (!current.left) {
+            current.left = newNode;
+            break;
+          }
+          current = current.left;
+        } else if (value > current.value) {
+          if (!current.right) {
+            current.right = newNode;
+            break;
+          }
+          current = current.right;
+        } else {
+          // Ignore duplicate values
+          break;
+        }
+      }
+    }
+  }
