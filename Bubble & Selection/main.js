359c24daabcd313c74af34b6c77bdfe3a13c035f
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-14 13:35:11 +0100

    Selection Sort for an Array:

--- Bubble & Selection/main.js
+++ Bubble & Selection/main.js
@@ -62,3 +62,26 @@ function bubbleSortArray(arr) {
     
     return head;
   }
+
+  //Selection Sort for an Array:
+  function selectionSortArray(arr) {
+    var len = arr.length;
+    
+    for (var i = 0; i < len - 1; i++) {
+      var minIndex = i;
+      
+      for (var j = i + 1; j < len; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j;
+        }
+      }
+      
+      if (minIndex !== i) {
+        var temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+      }
+    }
+    
+    return arr;
+  }
