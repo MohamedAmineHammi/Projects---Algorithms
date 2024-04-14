79ef25c30df8dd1474be611161e4b2954a82c0c1
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-14 13:33:51 +0100

    Bubble Sort for a Singly Linked List:

--- Bubble & Selection/main.js
+++ Bubble & Selection/main.js
@@ -25,3 +25,40 @@ function bubbleSortArray(arr) {
   var unsortedArray = [5, 3, 8, 2, 1, 4];
   var sortedArray = bubbleSortArray(unsortedArray);
   console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 8]
+
+  //Bubble Sort for a Singly Linked List:
+  function bubbleSortLinkedList(head) {
+    if (!head || !head.next) {
+      return head;
+    }
+    
+    var sorted = false;
+    while (!sorted) {
+      sorted = true;
+      var prev = null;
+      var current = head;
+      var next = head.next;
+      
+      while (next) {
+        if (current.val > next.val) {
+          if (prev) {
+            prev.next = next;
+          } else {
+            head = next;
+          }
+          
+          current.next = next.next;
+          next.next = current;
+          prev = next;
+          next = current.next;
+          sorted = false;
+        } else {
+          prev = current;
+          current = next;
+          next = next.next;
+        }
+      }
+    }
+    
+    return head;
+  }
