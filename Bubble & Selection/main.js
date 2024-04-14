574a5fa6f2a9ad2c45f4438f32a4cc90f0ca3ad0
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-14 13:33:06 +0100

    Bubble Sort for an Array:

---
+++ Bubble & Selection/main.js
@@ -0,0 +1,27 @@
+//Bubble Sort for an Array:
+function bubbleSortArray(arr) {
+    var len = arr.length;
+    var swapped;
+    
+    do {
+      swapped = false;
+      
+      for (var i = 0; i < len - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+          var temp = arr[i];
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
+          swapped = true;
+        }
+      }
+      
+      len--;
+    } while (swapped);
+    
+    return arr;
+  }
+  
+  // Example usage:
+  var unsortedArray = [5, 3, 8, 2, 1, 4];
+  var sortedArray = bubbleSortArray(unsortedArray);
+  console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 8]
