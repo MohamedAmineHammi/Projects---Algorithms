16fe57888b9b9277676f78d4ed07dc3aa8fc1f41
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-15 16:53:10 +0100

    Array: Combine

--- Sorts To Do 1/main.js
+++ Sorts To Do 1/main.js
@@ -18,3 +18,26 @@ function insertionSort(arr) {
   const array = [5, 3, 8, 2, 1];
   insertionSort(array);
   console.log(array);
+
+  //Array: Combine
+  function combineArrs(arr1, arr2) {
+    let i = arr1.length - 1;
+    let j = arr2.length - 1;
+    let k = arr1.length + arr2.length - 1;
+  
+    while (j >= 0) {
+      if (i >= 0 && arr1[i] > arr2[j]) {
+        arr1[k] = arr1[i];
+        i--;
+      } else {
+        arr1[k] = arr2[j];
+        j--;
+      }
+      k--;
+    }
+  }
+  
+  const arr1 = [1, 3, 5, 7];
+  const arr2 = [2, 4, 6];
+  combineArrs(arr1, arr2);
+  console.log(arr1);
