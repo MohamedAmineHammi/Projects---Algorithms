840c17f7177e994254f7ad74ced526729111071d
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-17 10:58:25 +0100

    Merge Sort

--- Sorts To Do 3/main.js
+++ Sorts To Do 3/main.js
@@ -26,3 +26,41 @@ function partitionArray(arr, start, end) {
   const array = [5, 1, 8, 4, 9, 2, 5, 3];
   quickSortArr(array);
   console.log(array); // Output: [1, 2, 3, 4, 5, 5, 8, 9]
+
+  //Merge Sort
+  function combineArrs(leftArr, rightArr) {
+    const combinedArr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+    while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+      if (leftArr[leftIndex] < rightArr[rightIndex]) {
+        combinedArr.push(leftArr[leftIndex]);
+        leftIndex++;
+      } else {
+        combinedArr.push(rightArr[rightIndex]);
+        rightIndex++;
+      }
+    }
+    
+    return combinedArr.concat(leftArr.slice(leftIndex)).concat(rightArr.slice(rightIndex));
+  }
+  
+  function mergeSortArr(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+    
+    const middle = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, middle);
+    const rightArr = arr.slice(middle);
+    
+    const sortedLeftArr = mergeSortArr(leftArr);
+    const sortedRightArr = mergeSortArr(rightArr);
+    
+    return combineArrs(sortedLeftArr, sortedRightArr);
+  }
+  
+  const array = [5, 1, 8, 4, 9, 2, 5, 3];
+  const sortedArray = mergeSortArr(array);
+  console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 5, 8, 9]
