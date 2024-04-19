8c8abe636cff5cad550d80edb63b71246caa9e4e
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-19 23:31:53 +0100

    Merge Sorted Arrays

--- Sets To Do 1/main.js
+++ Sets To Do 1/main.js
@@ -19,3 +19,37 @@ function interleaveArrays(arr1, arr2) {
   const array2 = [2, 6, 7, 2, 6, 2];
   const interleavedArray = interleaveArrays(array1, array2);
   console.log(interleavedArray);
+
+  //Merge Sorted Arrays
+  function mergeSortedArrays(arr1, arr2) {
+    const result = [];
+    let i = 0;
+    let j = 0;
+  
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] <= arr2[j]) {
+        result.push(arr1[i]);
+        i++;
+      } else {
+        result.push(arr2[j]);
+        j++;
+      }
+    }
+  
+    while (i < arr1.length) {
+      result.push(arr1[i]);
+      i++;
+    }
+  
+    while (j < arr2.length) {
+      result.push(arr2[j]);
+      j++;
+    }
+  
+    return result;
+  }
+  
+  const sortedArray1 = [1, 2, 2, 2, 7];
+  const sortedArray2 = [2, 2, 6, 6, 7];
+  const mergedArray = mergeSortedArrays(sortedArray1, sortedArray2);
+  console.log(mergedArray);
