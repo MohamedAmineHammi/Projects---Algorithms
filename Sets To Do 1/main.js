33ce6d263eadf45597013a0b446de1815375ae4c
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-19 23:32:34 +0100

    Minimal Three-Array Range

--- Sets To Do 1/main.js
+++ Sets To Do 1/main.js
@@ -53,3 +53,44 @@ function interleaveArrays(arr1, arr2) {
   const sortedArray2 = [2, 2, 6, 6, 7];
   const mergedArray = mergeSortedArrays(sortedArray1, sortedArray2);
   console.log(mergedArray);
+
+  //Minimal Three-Array Range
+  function minimalThreeArrayRange(arr1, arr2, arr3) {
+    let minRange = Infinity;
+    let minRangeObj = {};
+  
+    let i = 0;
+    let j = 0;
+    let k = 0;
+  
+    while (i < arr1.length && j < arr2.length && k < arr3.length) {
+      const minValue = Math.min(arr1[i], arr2[j], arr3[k]);
+      const maxValue = Math.max(arr1[i], arr2[j], arr3[k]);
+      const range = maxValue - minValue;
+  
+      if (range < minRange) {
+        minRange = range;
+        minRangeObj = { min: minValue, max: maxValue };
+      }
+  
+      if (minRange === 0) {
+        break;
+      }
+  
+      if (arr1[i] === minValue) {
+        i++;
+      } else if (arr2[j] === minValue) {
+        j++;
+      } else {
+        k++;
+      }
+    }
+  
+    return minRangeObj;
+  }
+  
+  const array1 = [1, 2, 4, 15];
+  const array2 = [3, 10, 12];
+  const array3 = [5, 10, 13, 17, 23];
+  const range = minimalThreeArrayRange(array1, array2, array3);
+  console.log(range);
