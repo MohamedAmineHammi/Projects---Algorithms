72c4fef41820db8181d348b8ddf0eac4655cb1a8
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-22 16:49:53 +0100

    Union Unsorted Arrays (no duplicates)

--- Set Theory/main.js
+++ Set Theory/main.js
@@ -2,3 +2,9 @@
 function unionInPlace(arr1, arr2) {
     arr1.push(...arr2);
   }
+
+  // Union Unsorted Arrays (no duplicates)
+function unionNoDuplicates(arr1, arr2) {
+    const set = new Set([...arr1, ...arr2]);
+    return Array.from(set);
+  }
