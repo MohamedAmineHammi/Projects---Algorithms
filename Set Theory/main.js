b265a16066f069f82732cff923159ae5bdacabd3
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-22 16:50:41 +0100

    Subset Unsorted Arrays

--- Set Theory/main.js
+++ Set Theory/main.js
@@ -23,3 +23,16 @@ function subsetSorted(arr1, arr2) {
   
     return j === arr2.length;
   }
+
+  // Subset Unsorted Arrays
+function subsetUnsorted(arr1, arr2) {
+    const set = new Set(arr1);
+  
+    for (let i = 0; i < arr2.length; i++) {
+      if (!set.has(arr2[i])) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
