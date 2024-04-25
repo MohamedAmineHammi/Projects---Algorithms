4f5b0609a0b3c507d6b01f3879752374b3eeda1f
MohamedAmineHammi <eminehammi@gmail.com>
2024-04-25 09:29:53 +0100

    Heap: Extract

---
+++ Sets To Do 5/main.js
@@ -0,0 +1,56 @@
+// Heap: Extract
+class MinHeap {
+    constructor() {
+      this.heap = [];
+    }
+  
+    extract() {
+      if (this.heap.length === 0) {
+        return null;
+      }
+  
+      const minValue = this.heap[0];
+      const lastValue = this.heap.pop();
+  
+      if (this.heap.length > 0) {
+        this.heap[0] = lastValue;
+        this.siftDown(0);
+      }
+  
+      return minValue;
+    }
+  
+    siftDown(index) {
+      const length = this.heap.length;
+      const element = this.heap[index];
+  
+      while (true) {
+        let leftChildIndex = 2 * index + 1;
+        let rightChildIndex = 2 * index + 2;
+        let swapIndex = null;
+  
+        if (leftChildIndex < length) {
+          if (this.heap[leftChildIndex] < element) {
+            swapIndex = leftChildIndex;
+          }
+        }
+  
+        if (rightChildIndex < length) {
+          if (
+            (swapIndex === null && this.heap[rightChildIndex] < element) ||
+            (swapIndex !== null && this.heap[rightChildIndex] < this.heap[leftChildIndex])
+          ) {
+            swapIndex = rightChildIndex;
+          }
+        }
+  
+        if (swapIndex === null) {
+          break;
+        }
+  
+        this.heap[index] = this.heap[swapIndex];
+        this.heap[swapIndex] = element;
+        index = swapIndex;
+      }
+    }
+  }
