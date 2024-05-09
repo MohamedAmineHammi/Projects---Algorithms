9f3da5888e0ec64c49ee5c2c6c4483a6d11f6f6c
MohamedAmineHammi <eminehammi@gmail.com>
2024-05-09 09:05:05 +0100

    Minding the Gap

--- Spatial Problems/main.js
+++ Spatial Problems/main.js
@@ -4,3 +4,10 @@ function distanceBetweenPoints2D(x1, y1, x2, y2) {
     const dy = y2 - y1;
     return Math.sqrt(dx * dx + dy * dy);
   }
+
+  function distanceBetweenPoints3D(x1, y1, z1, x2, y2, z2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    const dz = z2 - z1;
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+  }
