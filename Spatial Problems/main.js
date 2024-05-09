aff7590bfa1b8d4dd1b4a230a49bf243ae0e0752
MohamedAmineHammi <eminehammi@gmail.com>
2024-05-09 09:05:38 +0100

    Detecting the Overlap

--- Spatial Problems/main.js
+++ Spatial Problems/main.js
@@ -11,3 +11,19 @@ function distanceBetweenPoints2D(x1, y1, x2, y2) {
     const dz = z2 - z1;
     return Math.sqrt(dx * dx + dy * dy + dz * dz);
   }
+
+  //Detecting the Overlap
+  function checkRectangleOverlap(rect1, rect2) {
+    const [x1, y1, width1, height1] = rect1;
+    const [x2, y2, width2, height2] = rect2;
+  
+    if (x1 > x2 + width2 || x2 > x1 + width1) {
+      return false; // Rectangles do not overlap horizontally
+    }
+  
+    if (y1 > y2 + height2 || y2 > y1 + height1) {
+      return false; // Rectangles do not overlap vertically
+    }
+  
+    return true; // Rectangles overlap
+  }
